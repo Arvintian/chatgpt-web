c14bba5b38a813a9e2cdf73ddc0052ae99ca5ba0
Arvin <arvintian8@gmail.com>
2023-03-02 20:12:37 +0800

    add server

--- cmd/main.go
+++ cmd/main.go
@@ -1,38 +1,90 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"time"
 
 	"github.com/Arvintian/go-utils/cmdutil"
+	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
-	"k8s.io/klog/v2"
 )
+
+type ChatGPTWebServer struct {
+	Host              string `name:"host" usage:"http bind host" default:"0.0.0.0"`
+	Port              int    `name:"port" usage:"http bind port" default:"7080"`
+	BasicAuthUser     string `name:"auth-user" env:"BASIC_USER" usage:"http basic auth user"`
+	BasicAuthPassword string `name:"auth-password" env:"BASIC_PASSWORD" usage:"http basic auth password"`
+	BackendServer     string `name:"backend-server" default:"http://127.0.0.1:3002" usage:"backend's server endpoint"`
+	Version           bool   `name:"version" usage:"show version"`
+}
 
 var Version = "0.0.0-dev"
 
-type RootCommand struct {
-	Host    string `name:"host" usage:"bind host" default:"0.0.0.0"`
-	Port    int    `name:"port" usage:"bind port" default:"8000"`
-	Version bool   `name:"version" usage:"show version"`
-}
-
-func (r *RootCommand) Run(cmd *cobra.Command, args []string) error {
+func (r *ChatGPTWebServer) Run(cmd *cobra.Command, args []string) error {
 	if r.Version {
 		return r.ShowVersion()
 	}
-	klog.Infof("Run on %s:%d", r.Host, r.Port)
+	if r.BackendServer == "" {
+		fmt.Printf("Version: %s\n\n", Version)
+		return cmd.Help()
+	}
+	go r.httpServer(cmd.Context())
+
 	<-cmd.Context().Done()
 	return nil
 }
 
-func (r *RootCommand) ShowVersion() error {
+func (r *ChatGPTWebServer) httpServer(ctx context.Context) {
+	serverURL, err := url.Parse(r.BackendServer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	serverProxy := httputil.NewSingleHostReverseProxy(serverURL)
+
+	//proxy to upstream
+	server := &http.Server{
+		Addr: fmt.Sprintf("%s:%d", r.Host, r.Port),
+	}
+	entry := gin.New()
+	entry.Use(gin.Logger())
+	entry.Use(gin.Recovery())
+	if len(r.BasicAuthUser) > 0 {
+		entry.Use(gin.BasicAuth(gin.Accounts{
+			r.BasicAuthUser: r.BasicAuthPassword,
+		}))
+	}
+	entry.NoRoute(func(ctx *gin.Context) {
+		serverProxy.ServeHTTP(ctx.Writer, ctx.Request)
+	})
+
+	//listen and serve
+	server.Handler = entry
+	go func(ctx context.Context) {
+		<-ctx.Done()
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("Server shutdown with error %v", err)
+		}
+	}(ctx)
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatalf("Server listen and serve error %v", err)
+	}
+}
+
+func (r *ChatGPTWebServer) ShowVersion() error {
 	fmt.Println(Version)
 	return nil
 }
 
 func main() {
-	root := cmdutil.Command(&RootCommand{}, cobra.Command{
-		Long: "chatgpt-web",
+	root := cmdutil.Command(&ChatGPTWebServer{}, cobra.Command{
+		Long: "ChatGPT Web Server",
 	})
 	cmdutil.Main(root)
 }
--- go.mod
+++ go.mod
@@ -4,12 +4,36 @@ go 1.18
 
 require (
 	github.com/Arvintian/go-utils v0.0.0-20221012040808-2e61c0c3eece
+	github.com/gin-gonic/gin v1.9.0
 	github.com/spf13/cobra v1.6.0
-	k8s.io/klog/v2 v2.60.1
 )
 
 require (
+	github.com/bytedance/sonic v1.8.0 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.11.2 // indirect
+	github.com/goccy/go-json v0.10.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.9 // indirect
+	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
+	golang.org/x/crypto v0.5.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/klog/v2 v2.60.1 // indirect
 )
